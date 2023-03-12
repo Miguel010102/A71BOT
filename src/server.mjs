2761aa4501a61814586db4783025a4af6c9ea44b
Miguel010102 <78313488+Miguel010102@users.noreply.github.com>
2023-03-12 19:39:07 +0200

    Fixed

--- src/server.mjs
+++ src/server.mjs
@@ -1,31 +1,37 @@
 import { Client, GatewayIntentBits, SlashCommandBuilder, Routes } from 'discord.js';
 import { config } from 'dotenv';
-const { REST } = require('@discordjs/rest');
 
 
 config();
 
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages', GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, REST] });
+const client = new Client({ intents: ['Guilds', 'GuildMessages', GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 //Loading token before login is faster than loading it in the process.
 //Also loading the token from .env file, which is safer.
 const TOKEN = process.env.BOT_TOKEN
+const PREFIX = process.env.BOT_PREFIX
 
 client.login(TOKEN)
 
-const rest = new REST({ version: '9' }).setToken(TOKEN);
 
 client.on('ready', () => {
     console.log(`${client.user.tag} has logged in`);
 })
 
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.GUILD_ID;
+/*
+por si quieres reusar esto
 
-const commands = [
-    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong')
-];
+client.on(`messageCreate`, (message) => {
+    if (message.content == PREFIX + 'comando') {
+        message.channel.send("responde");
+    }
+});
+
+*/
 
 
-rest.put(Routes.applicationGuildCommand(clientId, guildId, { body: commands }))
-    .then(() => console.log('Succesfully registered application commands'));
+client.on(`messageCreate`, (message) => {
+    if (message.content == PREFIX + 'patata') {
+        message.channel.send("sisi dame patata caliente papi");
+    }
+});
