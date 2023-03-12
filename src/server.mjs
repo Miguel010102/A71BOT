ec53b8eaac2bd4c6206191faee4a890a33872e98
Miguel010102 <78313488+Miguel010102@users.noreply.github.com>
2023-03-12 10:11:13 +0200

    Fixed login
    Made the token and client_id get stored separately, you have to use new dependencies now but its worth it, but dont worry, they are localy installed inside the repository.

---
+++ src/server.mjs
@@ -0,0 +1,23 @@
+import { config } from 'dotenv'
+import { Client, GatewayIntentBits } from 'discord.js';
+
+config();
+
+const commands = [
+    {
+        name: 'ping',
+        description: 'Replies with Pong!',
+    },
+];
+
+const client = new Client({ intents: ['Guilds', 'GuildMessages', GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+//Loading token before login is faster than loading it in the process.
+//Also loading the token from .env file, which is safer.
+const TOKEN = process.env.BOT_TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
+
+client.login(TOKEN)
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in`);
+})
